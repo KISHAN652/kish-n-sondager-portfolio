3372a01c97590e3b7722739f7a158f4975119c68
sondager kishan <gajjarsk111@gmail.com>
2025-07-19 14:39:29 +0530

    Update taiwind.config.ts

--- taiwind.config.ts
+++ taiwind.config.ts
@@ -6,9 +6,6 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 export default {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
 	],
   theme: {
